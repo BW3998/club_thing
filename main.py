ab7393ac2102bc15e946cddb2a507b5f515c90a2
BW <88119091+BW3998@users.noreply.github.com>
2024-03-05 20:13:59 +0000

    number guessing game

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+from random import randrange
+ans = randrange(1,101)
+print("Welcome! You have 6 chances to guess my secret number!")
+guess = input("What is your first guess? ")
+i = 0
+while i < 6 and guess != ans:
+    guess = input("That's not right, try again!")
+if guess == ans:
+    print("You got it! Great job!")
+else:
+    print("Try again next time!")
